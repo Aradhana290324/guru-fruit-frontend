1e225fe6ae60acf47f4aadeb5473f52fab3d2112
Aradhana Gupta <dev@alphacore.co.in>
2026-06-01 16:45:16 +0530

    updated api base url to railway

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -2163,7 +2163,7 @@ maxWidth: "100%",
     fruit.imageUrl && (
 
         <img
-            src={`http://localhost:8080${fruit.imageUrl}`}
+            src={`https://spring-boot-fruits-production.up.railway.app${fruit.imageUrl}`}
             alt="fruit"
             style={{
                 width: "100%",
