6e3802f4531712e9c069471d819c2ceb21e6df56
Aradhana Gupta <dev@alphacore.co.in>
2026-06-01 18:23:52 +0530

    updated api base url to railway

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,6 +1,16 @@
+// import axios from "axios";
+
+// export default axios.create({
+//      // baseURL: "http://localhost:8080/api"
+//      baseURL:"https://spring-boot-fruits-production.up.railway.app"
+// });
 import axios from "axios";
 
-export default axios.create({
-     // baseURL: "http://localhost:8080/api"
-     baseURL:"https://spring-boot-fruits-production.up.railway.app"
+const api = axios.create({
+    baseURL: "https://spring-boot-fruits-production.up.railway.app",
+    headers: {
+        "Content-Type": "application/json"
+    }
 });
+
+export default api;
