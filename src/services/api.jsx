a92121629d5fe33c4c7b524981bb681b41278098
Aradhana Gupta <dev@alphacore.co.in>
2026-06-02 12:04:45 +0530

    file erase karne fir ye add kiya h code

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -7,10 +7,12 @@
 import axios from "axios";
 
 const api = axios.create({
+    
     baseURL: "https://spring-boot-fruits-production.up.railway.app/api",
     headers: {
         "Content-Type": "application/json"
     }
+    
 });
-
+console.log("UPLOAD RESPONSE =", response.data);
 export default api;
